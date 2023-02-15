0cd4dc74b2ef825f9971b87228580562718c061d
samdev <samstore637@gmail.com>
2023-02-15 23:48:10 +0300

    timeline

--- src/pages/process/info.js
+++ src/pages/process/info.js
@@ -1,3 +1,5 @@
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+
 export const paragraphSection = [
 	`
 		Innovation, according to Henriot, discovers firms that will favorably impact the world in the future. 
@@ -10,78 +12,99 @@ export const paragraphSection = [
 	`
 ]
 
+const iconStyles = {}
+const contentBoxStyle = {
+	background: "#DFE3E8"
+}
+
+const arrowStyle = { borderRight: "#DFE3E8" }
+
 export const timelineItems = [
 	{
-		title: "Task report - uploaded weekly reports",
-		text:"Added 3 files to task",
-		images: [
-			{
-				src: "/static/mock-images/covers/cover_5.jpg",
-				alt:"cover"
-			},
-			{
-				src: "/static/mock-images/covers/cover_6.jpg",
-				alt:"cover"
-			},
-			{
-				src: "/static/mock-images/covers/cover_11.jpg",
-				alt:"cover"
-			},
+		title: "Top-Down: Ideation",
+		paragraphs:[
+			`
+				Research and investment at Henriot begin with a top-to-bottom examination of the world's 
+				evolving trends and where they are likely to take us. Henriot uses an open research ecosystem 
+				to acquire knowledge on rapidly changing innovation topics, 
+				which aids in defining and refining the company's internal research process.
+			`,
+			`
+				These include experts in their areas, social media exchanges, and responses to Henriot's 
+				public research.
+			`
 		],
-		files:[],
-		date: "3 hrs ago"
+		images: "",
+		icon: <FaArrowDown style ={iconStyles}/>,
+		contentStyle: contentBoxStyle,
+		arrowStyle:arrowStyle
 	},
 	{
-		title: "Project status updated",
-		text:"Marked  FR-6 as completed",
-		images: [],
-		files:[],
-		date: "yesterday"
-	},
-	{
-		title: "New card styles images added",
-		text:"Added 3 card to Payments",
-		images: [
-			{
-				src: "/static/mock-images/covers/cover_5.jpg",
-				alt:"cover"
-			},
-			{
-				src: "/static/mock-images/covers/cover_6.jpg",
-				alt:"cover"
-			},
-			{
-				src: "/static/mock-images/covers/cover_11.jpg",
-				alt:"cover"
-			},
+		title: "Top-Down: Sizing The Opportunity",
+		paragraphs:[
+			`
+				It takes Henriot years of iterative research to predict and quantify 
+				long-term value chain changes and market opportunities. 
+				Technology-enabled disruption can be detected via Henriot's model of cost 
+				curves and calculations of demand elasticity. At this point, we begin our 
+				bottom-up approach, which identifies the firms that are most likely to profit.
+			`
 		],
-		files:[],
-		date: "october 11"
+		images: "",
+		icon: <FaArrowDown style ={iconStyles}/>,
+		contentStyle: contentBoxStyle,
+		arrowStyle:arrowStyle
 	},
 	{
-		title: "Dean added a new team member",
-		text:"Added a new member to Front Dashboard",
-		images: [
-			{
-				src: "/static/mock-images/avatars/avatar_5.jpg",
-				alt:"cover"
-			},
+		title: "Bottom-Up: Stock Selection And Valuation",
+		paragraphs:[
+			`
+				When Henriot does his bottom-up analysis, he starts with a small 
+				subset of possible investments rather than a standard. Henriot analyses 
+				possible investments using key indicators to quantify the businesses based on the 
+				possibility.
+			`,
+			`
+				Over the next five years, this will include developing a 
+				value and revenue model for each company in the portfolio. 
+				These models consider the company's unit volume growth; cost decreases, 
+				market acceptance, penetration, share count increase, and future multiples. 
+				All investment choices are ultimately Ken's to make since he serves as the company's 
+				Chief Investment Officer and portfolio manager.
+			`
 		],
-		files:[],
-		date: "october 05"
+		images: "",
+		icon: <FaArrowUp style ={iconStyles}/>,
+		contentStyle: contentBoxStyle,
+		arrowStyle:arrowStyle
 	},
 	{
-		title: "Project status updated",
-		text:"Marked  FR-3 as 'In progress'",
-		images: [],
-		files:[],
-		date: "october 01"
-	},
-	{
-		title: "Achievements",
-		text:"Earned a 'Top endorsed' badge",
-		images: [],
-		files:[],
-		date: "september 25"
+		title: "Bottom-Up: Portfolio and Risk Management",
+		paragraphs:[
+			`
+				During weekly portfolio and research meetings, Henriot closely checks each 
+				company's fundamental investment thesis.
+			`,
+			`
+				(i) We take advantage of opportunities created by short-term negative market 
+				actions or market sentiment;
+			`,
+			`
+				(ii) provide liquidity to invest in companies in which Henriot has a 
+				greater degree of confidence;
+			`,
+			`
+				or (iii) fund names that Henriot believes 
+				offer a relatively greater market opportunity relative to the current price. 
+				For example, suppose Henriot's measurements don't support a specific size or 
+				consider that a firm is no longer on the cutting edge of innovation. 
+				In that case, we may decide to sell the company. An Open Research Ecosystem 
+				Fuels Henriot's Investment Process
+			`
+		],
+		images: "",
+		icon: <FaArrowUp style ={iconStyles}/>,
+		contentStyle: contentBoxStyle,
+		arrowStyle:arrowStyle
 	},
 ]
