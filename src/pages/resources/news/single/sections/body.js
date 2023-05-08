0980b845c9e2fb6277b5a3c160ec092d5e021e1d
samdev <samstore637@gmail.com>
2023-05-08 13:37:07 +0300

    chancged custom font

--- src/pages/resources/news/single/sections/body.js
+++ src/pages/resources/news/single/sections/body.js
@@ -1,51 +1,62 @@
 import { Box, Container, Stack, Typography } from "@mui/material";
-import { styled } from "@mui/system"
+import { styled } from "@mui/system";
 
 import { connect } from "react-redux";
 
 const StyledWrapper = styled(Container)(({ theme }) => ({
 	minHeight: "60vh",
 	paddingTop: "30px",
-	paddingBottom: "30px"
-}))
+	paddingBottom: "30px",
+}));
+
+const customFont = { fontFamily: "'Times New Roman Custom', serif" };
 
 const StyledContentStack = styled(Stack)(({ theme }) => ({
-	
-}))
+}));
 
-const NewsBody = ({article}) => {
+const NewsBody = ({ article }) => {
+
 	return (
 		<StyledWrapper maxWidth="lg">
-			<Stack direction="column" spacing={3}>{console.log("Blocks", article.blocks)}
-				{
-					article.blocks.map((el, i) => (
-						<StyledContentStack key={i} spacing={1.5}>
-							{el.title ? (
-								<Typography variant="h4" color="text.primary">
-									{el.title}
-								</Typography>
-							) : ""}
-							{el.subtitle ? (
-								<Typography variant="h6" color="text.primary">
-									{el.subtitle}
-								</Typography>
-							) : ""}
-							{el.paragraphs ? el.paragraphs.map((par, i) => (
-								<Typography variant="subtitle2" color="text.secondary" key={i}>
-									{par}
-								</Typography>
-							)) : ""}
-						</StyledContentStack>
-					))
-				}
+			<Stack direction="column" spacing={3}>
+				
+				{article.blocks.map((el, i) => (
+					<StyledContentStack key={i} spacing={1.5}>
+						{el.title ? (
+							<Typography variant="h4" color="text.primary">
+								{el.title}
+							</Typography>
+						) : (
+							""
+						)}
+						{el.subtitle ? (
+							<Typography variant="h6" color="text.primary">
+								{el.subtitle}
+							</Typography>
+						) : (
+							""
+						)}
+						{el.paragraphs
+							? el.paragraphs.map((par, i) => (
+									<Typography
+										variant="h5"
+										color="text.secondary"
+										key={i}
+										sx={{ fontWeight: "500", fontFamily: customFont}}
+									>
+										{par}
+									</Typography>
+							  ))
+							: ""}
+					</StyledContentStack>
+				))}
 			</Stack>
 		</StyledWrapper>
-	)
-}
+	);
+};
 
+const mapStateToProps = ({ article }) => ({
+	article: article.article,
+});
 
-const mapStateToProps = ({article}) => ({
-	article : article.article
-})
-
-export default connect(mapStateToProps)(NewsBody)
+export default connect(mapStateToProps)(NewsBody);
