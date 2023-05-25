5070190b1ea85d8cba4b78d71fd986575b970915
samdev <samstore637@gmail.com>
2023-05-25 17:03:27 +0300

    modification

--- src/layout/footer/info.js
+++ src/layout/footer/info.js
@@ -2,49 +2,42 @@ import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export const privacyPolicy = [
 	`
-		You are strongly advised to obtain independent financial, legal, and tax advice 
-		before proceeding with any currency or spot metals trade. Nothing in this site 
-		should be read or construed as constituting advice on the part of The Henriot Group, 
-		or any of its affiliates, directors, officers, or employees. 
+		This site is not intended for use in jurisdictions in which the trading or investments described are prohibited and should only be used by such persons and in such ways as are legally permitted.
 	`,
 	`
-		By accessing this site, you agree that the laws of the Republic of Seychelles, 
-		without regard to Conflict of Laws principles thereof, will apply to all matters 
-		relating to the use of this site. In case of a dispute, you agree to the exclusive 
-		jurisdiction of the Courts of the Republic of Seychelles.
-	`
-]
+	Your investment may not qualify for investor protection in your country or state of residence, so please conduct your own due diligence. This website is free for you to use but we may receive a commission from the companies we feature on this site FTC Compliance. All forms of trading carry a high level of risk so you should only speculate with money you can afford to lose. You can lose more than your initial deposit and stake. Please ensure your method matches your investment objectives, study the risks involved and if necessary seek independent advice. Henriot Investment Management will not accept any liability for loss or damage as a result of reliance on the information contained within this website including data, quotes, charts, and buy/sell setups (may be interpreted as signals are meant only as a reading of the indicators and system rules). Remember the risk of trading Forex & CFD – it’s one of the riskiest forms of investment. Risk warning: Trading foreign exchange or contracts for differences in margin carries a high level of risk and may not be suitable for all investors. You should ensure you understand all of the risks. Before using Henriot's services please acknowledge the risks associated with trading. Remember that software’s past performance does not ensure future results and you may lose some or all of your invested capital. Therefore, do not risk the capital you can not afford to lose.
+	`,
+];
 
 export const footerParagraph = [
 	`Henriot Investment Management (Investment fund) is alternative investment fund designed for Professional or Informed investors only and supervised by the Capital Markets Authority. The information contained in this webpage does not constitute a solicitation or offer to invest in any financial products. It does not constitute legal or fiscal advice nor does it provide specific investment advice.`,
 	`Detailed information about the fund can be found in the fund’s rules, prospectus and Key investor information document that can be obtained from HG Asset Management. The data has been compiled from sources deemed reliable and is accurate to the best of our knowledge; however, we accept no obligation whatsoever from the information given in this webpage.`,
 	`HG Asset Management assumes no (direct or indirect) liability for any losses which result from the data and information published herein. The offering and sale of the products described in this webpage is legally restricted in certain jurisdictions. Persons who are subordinate to such jurisdictions have to inform themselves about such restrictions and must adhere to them. `,
 	`The prices stated are net of all fees; front load and taxes have not been taken into account. The potential for high returns of this investment strategy is combined with significant risks. Even a total loss of the invested capital is possible. Performance from the past is no guarantee of future results. Funds management company is not liable for the losses of the fund suffered due to the fluctuations of financial markets. It is strongly recommended to overview Fund's Prospect and Articles of Association before making a decision to invest in the Fund.`,
-
-]
+];
 
 const styledIcon = {
-	fontSize: "16px"
-}
+	fontSize: "16px",
+};
 
 export const footerSocialMedia = [
 	{
-		icon: <FaFacebook style={styledIcon}/>,
-		link: "https://www.facebook.com"
+		icon: <FaFacebook style={styledIcon} />,
+		link: "https://www.facebook.com",
 	},
 	{
-		icon: <FaTwitter style={styledIcon}/>,
-		link: "https://twitter.com/"
+		icon: <FaTwitter style={styledIcon} />,
+		link: "https://twitter.com/",
 	},
 	{
-		icon: <FaInstagram style={styledIcon}/>,
-		link: "https://www.instagram.com/"
+		icon: <FaInstagram style={styledIcon} />,
+		link: "https://www.instagram.com/",
 	},
 	{
-		icon: <FaLinkedin style={styledIcon}/>,
-		link: "https://www.linkedin.com/"
+		icon: <FaLinkedin style={styledIcon} />,
+		link: "https://www.linkedin.com/",
 	},
-]
+];
 
 export const footerItems = [
 	{
@@ -52,71 +45,71 @@ export const footerItems = [
 		items: [
 			{
 				path: "/landing/exchange",
-				label: "FX Funds"
+				label: "FX Funds",
 			},
 			{
 				path: "/landing/cec",
-				label: "CEC Funds"
+				label: "CEC Funds",
 			},
 			{
 				path: "/landing/diverse",
-				label: "Diverse Funds"
+				label: "Diverse Funds",
 			},
-		]
+		],
 	},
 	{
 		title: "Investment Solution",
 		items: [
 			{
 				path: "/landing/investment-solutions/forex",
-				label: "Forex"
+				label: "Forex",
 			},
 			{
 				path: "/landing/investment-solutions/equity",
-				label: "Equity"
+				label: "Equity",
 			},
 			{
 				path: "/landing/investment-solutions/structures-products",
-				label: "Sctructured Product"
+				label: "Sctructured Product",
 			},
 			{
 				path: "/landing/investment-solutions/portfolios",
-				label: "Portfolio"
+				label: "Portfolio",
 			},
-		]
+		],
 	},
 	{
 		title: "Resources",
 		items: [
 			{
 				path: "/landing/resources/news",
-				label: "News"
+				label: "News",
 			},
 			{
 				path: "/landing/resources/news",
-				label: "REsearch"
+				label: "REsearch",
 			},
 			{
 				path: "/landing/resources/news",
-				label: "Learn"
+				label: "Learn",
 			},
-		]
+		],
 	},
 	{
 		title: "Company",
 		items: [
 			{
 				path: "/landing/company/overview",
-				label: "Overview"
+				label: "Overview",
 			},
 			{
 				path: "/landing/company/faq",
-				label: "FAQ"
+				label: "FAQ",
 			},
 			{
 				path: "/landing/company/careers",
-				label: "Careers"
+				label: "Careers",
 			},
-		]
+		],
 	},
-]
+];
--- src/pages/home/info.js
+++ src/pages/home/info.js
@@ -4,11 +4,11 @@ export const accordionInfo = [
 			Market Amplification
 		`,
 		paragraph: [
-			`Market Amplification through Consistency, Discipline and Objectivity Quantitative trading is characterized by its consistency, discipline, and objectivity. `,
+			`Market Amplification through Consistency, Discipline and Objectivity. Quantitative trading is characterized by its consistency, discipline, and objectivity. `,
 			`By using algorithms to process vast amounts of data, quantitative fund managers can make well-informed investment decisions in a systematic and repeatable manner.`,
 			`This not only provides a higher level of confidence in the decision-making process, but it also allows for better market timing. With the ability to identify patterns before they become mainstream, a quantitative approach can lead to superior returns.`,
 		],
-		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1676462807/henriot/cover3_xje8wj.jpg"
+		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1676462807/henriot/cover3_xje8wj.jpg",
 	},
 	{
 		title: `
@@ -17,9 +17,9 @@ export const accordionInfo = [
 		paragraph: [
 			`Henriot Investment Management differs from any typical fund by using extensive diversification through various different financial instruments and applying multiple completely different trading strategies. Thousands of different strategy variations catch trends, react to market waves, find repetitive patterns and seasonalities. `,
 			`These strategies are used for more than 40 different futures contracts. Put together, they create numerous independent trading patterns. Currently more than 350 trading algorithms are used at once. They work on a daily to weekly basis. A single strategy will almost always suffer from periods of zero or negative returns. Having several hundred fundamentally different models in our portfolio allows us to compensate one’s losses with gains of other models.`,
-			`Therefore, whole fund performance is almost always positive with more than 70% of positive months, which results in an almost steady performance. However, risk is not entirely eliminated.`
+			`Therefore, whole fund performance is almost always positive with more than 70% of positive months, which results in an almost steady performance. However, risk is not entirely eliminated.`,
 		],
-		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816765/henriot/yiorgos-ntrahas-mcAUHlGirVs-unsplash_zos69m.jpg"
+		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816765/henriot/yiorgos-ntrahas-mcAUHlGirVs-unsplash_zos69m.jpg",
 	},
 	{
 		title: `
@@ -28,39 +28,38 @@ export const accordionInfo = [
 		paragraph: [
 			`Human intervention can effectively distinguish alpha from noise Machine learning and Algo trading models has the potential to help investors find alpha in unexpected places.`,
 			`By analyzing financial data sets, machine learning models have in the past already identified the key performance factors that can increase the risk-reward ratio over time.`,
-			`However, while machines have proven to be effective in amplifying market movements, it is crucial to have a human element involved and a clear understanding of how the algorithms are making decisions.`	
-
+			`However, while machines have proven to be effective in amplifying market movements, it is crucial to have a human element involved and a clear understanding of how the algorithms are making decisions.`,
 		],
-		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816840/henriot/juliana-kozoski-IoQioGLrz3Y-unsplash_hcvy0v.jpg"
+		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816840/henriot/juliana-kozoski-IoQioGLrz3Y-unsplash_hcvy0v.jpg",
 	},
-]
+];
 
 export const homeAbout = [
 	`
 		Henriot identifies more innovations evolving today than ever before. We believe it is changing lives and businesses across the world dramatically, creating opportunities to own the next big thing by investing in the future today.
-	`, 
+	`,
 	`
 		Henriot identifies more innovations evolving today than ever before. We believe it is changing lives and businesses across the world dramatically, creating opportunities to own the next big thing by investing in the future today.
-	`
-]
+	`,
+];
 
 export const contentAfterBanner = [
 	`
-		The Fund runs conservative strategies that seeks stable capital appreciation in the mid to long term, with a low correlation to the performance of traditional asset classes.
+		HG develops automated and sophisticated trading solutions for both retail and institutional Clients. These strategies seek stable capital appreciation in the mid to long term, with a low correlation to the performance of traditional asset classes.
 	`,
 	`
 		Strict risk management principles are put in place to safeguard the fund and its investors. We place the highest priority in controlling our risk and losses to preserve capital and aim for long-term wealth accumulation.
 	`,
-]
+];
 
 export const researchCenter = [
 	{
 		type: "article",
 		category: "Analytic research",
 		title: "parcel drone delivery should supercharge ecommerce",
-		tags:  ["Electric Vehicles", "Autonomous Technology"],
+		tags: ["Electric Vehicles", "Autonomous Technology"],
 		readTime: "34 minutes",
-		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816840/henriot/juliana-kozoski-IoQioGLrz3Y-unsplash_hcvy0v.jpg"
+		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816840/henriot/juliana-kozoski-IoQioGLrz3Y-unsplash_hcvy0v.jpg",
 	},
 	{
 		type: "article",
@@ -68,38 +67,14 @@ export const researchCenter = [
 		title: "We believe zoom is the fabric connecting global enterprise",
 		tags: ["Electric Vehicles", "Autonomous Technology"],
 		readTime: "34 minutes",
-		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816765/henriot/yiorgos-ntrahas-mcAUHlGirVs-unsplash_zos69m.jpg"
+		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816765/henriot/yiorgos-ntrahas-mcAUHlGirVs-unsplash_zos69m.jpg",
 	},
 	{
 		type: "article",
 		category: "Analytic research",
 		title: "The Impact of Technology on Clinical Trial Pipeline Assets",
-		tags:  ["Electric Vehicles", "Autonomous Technology"],
+		tags: ["Electric Vehicles", "Autonomous Technology"],
 		readTime: "34 minutes",
-		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816662/henriot/chart-4065756_1920_j3h3zk.jpg"
+		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816662/henriot/chart-4065756_1920_j3h3zk.jpg",
 	},
-	{
-		type: "article",
-		category: "Analytic research",
-		title: "Global Digital Advertising Appears Poised for Significant Growth",
-		tags:  ["Electric Vehicles", "Autonomous Technology"],
-		readTime: "34 minutes",
-		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816662/henriot/chart-4065756_1920_j3h3zk.jpg"
-	},
-	{
-		type: "article",
-		category: "Analytic research",
-		title: "The Equity Market Cap Associated with Technologically Enabled Innovation is likely to Grow Exponentially",
-		tags:  ["Electric Vehicles", "Autonomous Technology"],
-		readTime: "34 minutes",
-		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816765/henriot/yiorgos-ntrahas-mcAUHlGirVs-unsplash_zos69m.jpg"
-	},
-	{
-		type: "article",
-		category: "Analytic research",
-		title: "Augmented Reality (AR) could Scale 1000-Fold From $ 1 Billion to $ 1Trillion",
-		tags:  ["Electric Vehicles", "Autonomous Technology"],
-		readTime: "34 minutes",
-		image: "https://res.cloudinary.com/dqweh6zte/image/upload/v1675816840/henriot/juliana-kozoski-IoQioGLrz3Y-unsplash_hcvy0v.jpg"
-	},
-]
+];
